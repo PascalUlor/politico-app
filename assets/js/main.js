4b9d46ab7c0b3caceb13291bfe3ffb3d2672d6eb
PascalUlor <pascalulor@yahoo.com>
2019-01-21 20:45:45 +0100

    ft(View Parties): Build page for user to view political parties [Delivers #163301342]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+/**
+ * Display Modal
+ * @param {string} id - The id of the modal
+ * @param {string} modal - The id of the modal container
+ */
+const toggleModal = (id, modal) => {
+    if (document.getElementById(modal).style.display === 'block') {
+      document.getElementById(modal).style.display = 'none';
+      document.getElementById(id).style.display = 'none';
+    } else {
+      document.getElementById(modal).style.display = 'block';
+      document.getElementById(id).style.display = 'block';
+    }
+  };
+
+
 /*
 *DIsplay Backdrop when mobile side bar is toggled
 *
